8544f03a6e108b352225c79e7ff567a2605701f4
cenlan <cenlan@hotmail.com>
2020-03-02 10:04:33 +0800

    添加unity对空指针的判定

--- network/export_to_unity.cpp
+++ network/export_to_unity.cpp
@@ -13,25 +13,38 @@ _DLLExport void init() {
   if (!unity_session_mgr)
     unity_session_mgr = new session_mgr(*ioc_ptr);
 }
-_DLLExport void run() { ioc_ptr->run_one(); }
+_DLLExport void run() {
+  if (ioc_ptr)
+    ioc_ptr->run_one();
+}
 _DLLExport void stop() {
-  delete unity_session_mgr;
-  unity_session_mgr = nullptr;
-  delete ioc_ptr;
-  ioc_ptr = nullptr;
+  if (unity_session_mgr) {
+    delete unity_session_mgr;
+    unity_session_mgr = nullptr;
+  }
+  if (ioc_ptr) {
+    delete ioc_ptr;
+    ioc_ptr = nullptr;
+  }
 }
 _DLLExport void disconnect(uint64 session_id) {
-  unity_session_mgr->remove_session(session_id);
+  if (unity_session_mgr)
+    unity_session_mgr->remove_session(session_id);
 }
 _DLLExport uint64 connect_server(char *host, char *port) {
+  if (!unity_session_mgr)
+    return 0;
   return unity_session_mgr->create_session(
       std::string("control"), std::string(host), std::string(port));
 }
 _DLLExport void send_msg(uint64 session_id, char *msg) {
-  unity_session_mgr->send_msg(session_id, std::string(msg));
+  if (unity_session_mgr)
+    unity_session_mgr->send_msg(session_id, std::string(msg));
 }
 
 _DLLExport bool session_valid(uint64 session_id) {
+  if (!unity_session_mgr)
+    return false;
   return unity_session_mgr->session_valid(session_id);
 }
 }
